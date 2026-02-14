77f9746c2809245fc9dc437764580f2428756e2b
Mike Fleming <mike.rcc@gmail.com>
2026-02-14 11:35:59 -0800

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+
+
+export class Index {
+
+  constructor(container: HTMLElement) {
+  }
+
+  public render(): void {
+    //  this.container.innerHTML = 'Hello World';
+  }
+
+  public init() {
+
+    console.log('in init init');
+
+    // let btn2 = document.querySelector('#scrollButton');
+    // if (btn2) {
+    //   btn2!.addEventListener('click', (e) => {
+    //     const input = document.querySelector("#scrollParam") as HTMLInputElement;
+    //     console.log(input.value);
+    //     grid.scroll(1);
+    //     // don't have to call - it's called from onScroll event grid.updateDebugOverlay();
+    //   });
+    // }
+
+  }
+
+} // end init
+
